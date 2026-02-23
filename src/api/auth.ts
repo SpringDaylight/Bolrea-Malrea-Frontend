de484d38f5b2f3be4821a7d094fecc2ff5a891bb
dyeoniii <rlaehdus0410@gmail.com>
2026-02-23 15:31:04 +0900

    modified auth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -95,8 +95,8 @@ export function signup(data: SignupRequest): Promise<AuthUserResponse> {
 /**
  * Local login
  */
-export function login(data: LoginRequest): Promise<AuthTokenResponse> {
-  return post<AuthTokenResponse>('/api/auth/login', data);
+export function login(data: LoginRequest): Promise<AuthUserResponse> {
+  return post<AuthUserResponse>('/api/auth/login', data);
 }
 
 /**

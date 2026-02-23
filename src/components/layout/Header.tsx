00326a2a241b0601f465e6c76347230a8adff358
dyeoniii <rlaehdus0410@gmail.com>
2026-02-23 21:47:38 +0900

    changed group and header states

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -24,6 +24,7 @@ export default function Header() {
     const isLoggedIn = Boolean(getAccessToken());
 
     if (isLoggedIn) {
+      setIsLoggedIn(true);
       setProfileHref("/mypage");
       setProfileLabel("DS");
       getCurrentUser()

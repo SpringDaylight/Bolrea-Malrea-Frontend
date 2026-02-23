10e3fc753097d30e31ee305c35ddb61919bc5bb4
dyeoniii <rlaehdus0410@gmail.com>
2026-02-23 11:41:14 +0900

    Mw 222 개발 (#40)
    * changed styles and added runtime filter
    
    * changed files: Group, movies, tasteanalysis, activity pages

--- src/pages/ActivityPage.tsx
+++ src/pages/ActivityPage.tsx
@@ -836,7 +836,7 @@ export default function ActivityPage() {
               </div> */}
             </div>
           </div>
-          {/* <p className="muted profile-bio profile-bio-below">"{profile.bio}"</p> */}
+          <p className="muted profile-bio profile-bio-below">"{profile.bio}"</p>
         </section>
 
           <section className="section card taste-preview-section activity-top-card">
@@ -1062,7 +1062,7 @@ export default function ActivityPage() {
                         </div>
                       </div>
                       <p className="muted review-summary-text review-summary-full">
-                        "{reviewText}"
+                        {reviewText}
                       </p>
                     </article>
                 </div>

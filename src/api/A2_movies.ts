b6f0d350ef569c13feeb70f765ca6642cdef9c82
dyeoniii <rlaehdus0410@gmail.com>
2026-02-24 16:32:29 +0900

    Mw 223 개발 (#67)
    * changed homepage component
    
    * changed home page's component location
    
    * changed header and home page, moviedetail page, tasteanalysispage and style
    
    * modified build error
    
    * modified build error
    
    * modified wordcloud states
    
    * changed movie's filter
    
    * modified components and css styles
    
    * modified build error
    
    * modified movie's array

--- src/api/A2_movies.ts
+++ src/api/A2_movies.ts
@@ -59,7 +59,7 @@ export function getMovies(params?: {
   query?: string;
   genres?: string;
   category?: string;
-  sort?: 'latest' | 'popular' | 'rating';
+  sort?: 'latest' | 'popular' | 'rating' | 'title';
   runtime_min?: number;
   runtime_max?: number;
   year_min?: number;
